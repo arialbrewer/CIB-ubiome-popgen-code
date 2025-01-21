883284aaadceca1e01757326e2dbf6d187ddc6f5
Arial Brewer <arial.brewer@gmail.com>
2025-01-21 13:38:20 -0800

    New repo for chapter 3

---
+++ CIB_popgen.R
@@ -0,0 +1,8 @@
+#Arial Brewer
+#PhD Chapter 3: CIB population genetics
+
+#load packages
+library(tidyverse)
+library(vegan)
+library(strataG)
+library(adegenet)
---
+++ CIB_ubiome.R
@@ -0,0 +1,7 @@
+#Arial Brewer
+#PhD Chapter 3: CIB epidermal microbiome
+
+#load packages
+library(tidyverse)
+library(vegan)
+library(phyloseq)
